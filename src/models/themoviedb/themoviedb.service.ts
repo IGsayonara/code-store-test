a566356a398a5ef26dab19c56c0686679a8c5ede
IGsayonara <diduniki@gmail.com>
2024-04-25 23:02:55 +0100

    feat: add dotenv for API key

--- src/models/themoviedb/themoviedb.service.ts
+++ src/models/themoviedb/themoviedb.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosResponse } from 'axios';
 import { ThePopularMoviesDbResponseDto } from './dto/themoviedb-popular.dto';
+import { TheFullMovieResponseDto } from './dto/thefullmoviedb.dto';
+import * as process from 'process';
+console.log(process.env.MOVIEDB_API_KEY);
 
 @Injectable()
 export class TheMovieDbService {
@@ -8,11 +11,22 @@ export class TheMovieDbService {
     try {
       const response: AxiosResponse<ThePopularMoviesDbResponseDto> =
         await axios.get(
-          'https://api.themoviedb.org/3/movie/popular?api_key=api_key',
+          `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.MOVIEDB_API_KEY}`,
         );
       return response.data;
     } catch (error) {
       throw new Error('Error fetching popular movies');
+    }
+  }
+
+  async fetchMovie(id: number): Promise<TheFullMovieResponseDto> {
+    try {
+      const response: AxiosResponse<TheFullMovieResponseDto> = await axios.get(
+        `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.MOVIEDB_API_KEY}`,
+      );
+      return response.data;
+    } catch (error) {
+      throw new Error(`Error fetching movie with id ${id}`);
     }
   }
 }
