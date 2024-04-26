1a1d8ce5b149f0c65e7c780b44a64db29507c72b
IGsayonara <diduniki@gmail.com>
2024-04-26 10:33:55 +0100

    feat: replace dotenv with config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MovieModule } from './models/movies/movie.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MovieModule],
+  imports: [
+    MovieModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // make the config module available globally
+      envFilePath: '.env', // specify the path to your environment file
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
