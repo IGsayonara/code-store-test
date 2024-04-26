3f01de0d6c025e9e87044e2ce5a13db8f9e9484d
IGsayonara <diduniki@gmail.com>
2024-04-26 10:55:55 +0100

    feat: remove unneeded files, define port from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MovieModule } from './models/movies/movie.module';
 import { ConfigModule } from '@nestjs/config';
 
@@ -12,7 +10,7 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: '.env', // specify the path to your environment file
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
